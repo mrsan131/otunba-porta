a2c9f1700fea0868bb28849c16c0538c20afa617
mrsan131 <santidominhuez9@gmail.com>
2024-10-30 18:57:20 -0500

    Delete .vercel/output directory

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/basta ya_RL-d2GFp.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./basta ya_Bplkb9fO.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
